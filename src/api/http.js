1d3ba715d82237da6158d3e8625415e917f3a8c2
서진 <dltjwls0529@gmail.com>
2023-02-23 17:57:58 +0900

    fix: s 추가

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const http = axios.create({
-	baseURL: "http://3.34.224.240:3000",
+	baseURL: "https://3.34.224.240:3000",
 });
 
 http.defaults.withCredentials = true;
